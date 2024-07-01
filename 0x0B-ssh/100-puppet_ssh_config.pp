23dc1b83c8891544634b23899d110287584228c7
jenanouar1234 <jenanouar1234@gmail.com>
2024-07-01 13:56:41 +0100

    pushed

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,25 +3,38 @@
 
 include stdlib
 
-file_line { 'Set SSH Private Key':
+file_line { 'SSH Private Key':
   path               => '/etc/ssh/ssh_config',
   line               => '    IdentityFile ~/.ssh/school',
-  match              => '^[#]*[\s]*(?i)IdentityFile[\s]+.*',
+  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
   replace            => true,
-  append_on_no_match => true,
-  notify             => Service['ssh'],
+  append_on_no_match => true
 }
 
-file_line { 'Deny Password Authentication':
+# Regex match explanation
+#
+# ^       beginning of the line
+# [#]*  atleast one hash character
+# [\s]*  zero or more white space characters
+# (?i)IdentityFile case insensitive "IdentityFile"
+# [\s]+ at least one whitespace character
+# ~/.ssh/id_rsa The ssh private key file path we want to replace
+# $      end of the line
+
+file_line { 'Deny Password Auth':
   path               => '/etc/ssh/ssh_config',
   line               => '    PasswordAuthentication no',
-  match              => '^[#]*[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
+  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
   replace            => true,
-  append_on_no_match => true,
-  notify             => Service['ssh'],
+  append_on_no_match => true
 }
 
-service { 'ssh':
-  ensure => 'running',
-  enable => true,
-}
+# Regex match explanation
+#
+# ^       beginning of the line
+# [#]*  atleast one hash character
+# [\s]*  zero or more white space characters
+# (?i)PasswordAuthentication case insensitive "PasswordAuthentication"
+# [\s]+ at least one whitespace character
+# (yes|no) with the value "yes" or the value "no"
+# $      end of the line
