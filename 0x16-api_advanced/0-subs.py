66c30aae53fb292332677f7891d9c8280155eff9
jenanouar1234 <jenanouar1234@gmail.com>
2024-08-16 13:19:47 +0100

    pushed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,21 +5,26 @@ Contains the number_of_subscribers function
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """returns the number of subscribers for a given subreddit"""
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
     url = f'http://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/Putrid-Search-6501)'}
+    headers = {
+        'User-Agent': (
+            '0x16-api_advanced:project:v1.0.0 (by /u/Putrid-Search-6501)'
+        )
+    }
 
     try:
         response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Check for HTTP errors
+        if response.status_code == 404:
+            return 0
+        response.raise_for_status()
         data = response.json()
         subs = data.get("data", {}).get("subscribers", 0)
         return subs
-    except (requests.RequestException, ValueError) as e:
-        # Handle HTTP and JSON errors
-        print(f"Error: {e}")
+    except (requests.RequestException, ValueError):
         return 0
